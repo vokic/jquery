6bdad622c218602b5637b01a4a6ede43594a6d5b
vokic <30930109+vokic@users.noreply.github.com>
2019-01-15 19:52:26 +0100

    Add files via upload

---
+++ nocdan.js
@@ -0,0 +1,7 @@
+$("button").on("click", function () {
+    $('body').toggleClass('crna-tema');
+}
+
+document.querySelector('button').addEventListener('click', function () {
+        document.body.classList.toggle('crna-tema');
+    })
